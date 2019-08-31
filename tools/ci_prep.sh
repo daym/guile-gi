dc40efb7423948b6741aea5c7e87eb21f44780e6
Michael Gran <spk121@yahoo.com>
2019-08-31 10:30:31 -0700

    getting warmer, who invented sh syntax anyway

--- tools/ci_prep.sh
+++ tools/ci_prep.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-if [ $1 == stable22 ];
-then
+if [ "$1" = "stable22" ]; then
     git clone --depth 1 https://git.savannah.gnu.org/git/guile.git --single-branch --branch=stable-2.2;
-elif [ $1 == master ];
-then
+elif [ "$1" = "master" ]; then
     git clone --depth 1 https://git.savannah.gnu.org/git/guile.git --single-branch;
 else
+    cat $1 > guile_build.txt
     exit 0;
 fi
 
-cd guile && ./autogen.sh && ./configure && make && sudo make install && cd ..
+cd guile && ./autogen.sh && ./configure && make && sudo make install && cd .. && cat $1 > guile_build.txt
 
 
     
