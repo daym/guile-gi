385ba521d6b2c16aa57dba44c963f3588acda98e
Michael Gran <spk121@yahoo.com>
2019-08-31 09:19:33 -0700

    try all guile versions

---
+++ tools/ci_prep.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ $1 == stable22 ];
+then
+    git clone --depth 1 https://git.savannah.gnu.org/git/guile.git --single-branch --branch=stable-2.2;
+elif [ $1 == master ];
+then
+    git clone --depth 1 https://git.savannah.gnu.org/git/guile.git --single-branch;
+else
+    exit 0;
+fi
+
+cd guile && ./autogen.sh && ./configure && make && sudo make install && cd ..
+
+
+    
